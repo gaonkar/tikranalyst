b04274a33caefaa30f9c72958956a908d5869bd0
gaonkar <gaonkar@users.noreply.github.com>
2024-01-01 00:06:34 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="gaonkar"
+original_name="tikranalyst"
+original_urlname="tikranalyst"
+original_description="Awesome tikranalyst created by gaonkar"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv tikranalyst $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
